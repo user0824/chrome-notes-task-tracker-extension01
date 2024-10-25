75a79a6e372404bc8f19f2fc99efcec257d3dd06
Amit <149993488+user0824@users.noreply.github.com>
2024-10-25 17:16:46 -0400

    added CSS Styling

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,7 @@
 console.log(test);
 
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+  const writeNote = document.querySelector('#writeNote');
+  const saveBtn = document.querySelector('#saveNote');
+  const addBtn = document.querySelector('#addNote');
+});
